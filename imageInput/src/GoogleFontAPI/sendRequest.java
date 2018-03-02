23861197055861b4e62d6a82221b7a00ba0183a8
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2018-03-02 13:03:37 -0500

    help me

---
+++ imageInput/src/GoogleFontAPI/sendRequest.java
@@ -0,0 +1,7 @@
+package GoogleFontAPI;
+
+public class sendRequest {
+	final protected String apikey = "AIzaSyAgJ2Fpuffe-V-3TkmdTzJwjQ9sooh7TgU";
+	
+}
+ 
--- imageInput/src/getImage/getImage.java
+++ imageInput/src/getImage/getImage.java
@@ -3,32 +3,49 @@ package getImage;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
-import java.awt.*;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 
 public class getImage
 {
-	private static Map<pixelPosition, RGBValue> pixels = new HashMap<pixelPosition, RGBValue>();
-	private static int width;
-	private static int height;
-	  public static void main(String args[]) throws IOException{
-	  File file= new File("your_file.jpg");
-	  BufferedImage image = ImageIO.read(file);
-	 
-	  width = image.getWidth();
-	  height = image.getHeight();
-	  
-	  for(int i = 0; i < height; i++) {
-		for(int j = 0; j < width; j++) {
-		  int pixel = image.getRGB(j, i);
-		  int alpha = (pixel >> 24) & 0xff;
-		  int red = (pixel >> 16) & 0xff;
-		  int green = (pixel >> 8) & 0xff;
-		  int blue = (pixel) & 0xff;
-		  pixels.put(new pixelPosition(j,i), new RGBValue(alpha,red,green,blue));
-		 }
-      }
-			
-	  }
+	final private static Map<pixelPosition, RGBValue> pixels = new HashMap<pixelPosition, RGBValue>();
+	final private int width;
+	final private int height;
+	
+	public Map<pixelPosition, RGBValue> getPixels() {
+		return pixels;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public getImage(String filepath) throws IOException
+	{
+		File file= new File(filepath);
+		BufferedImage image = ImageIO.read(file);
+		 
+		this.width = image.getWidth();
+		this.height = image.getHeight();
+		  
+		  for(int i = 0; i < this.height; i++) {
+			for(int j = 0; j < this.width; j++) {
+			  int pixel = image.getRGB(j, i);
+			  int alpha = (pixel >> 24) & 0xFF;
+			  int red = (pixel >> 16) & 0xFF;
+			  int green = (pixel >> 8) & 0xFF;
+			  int blue = (pixel) & 0xFF;
+			  //System.out.println(i + " , " + j + "    :   " + alpha + " , " + red + " , " + green + " , " + blue);
+			  pixels.put(new pixelPosition(j,i), new RGBValue(alpha,red,green,blue));
+			 }
+	      }
+	}
+    
+	public static void main(String args[]) throws IOException{
+		getImage test = new getImage("Twitter.png");	
+    }
 }
