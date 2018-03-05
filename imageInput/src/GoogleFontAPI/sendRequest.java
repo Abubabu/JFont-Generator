faac26596ee4e38a359ae241a2c30eef4663ada4
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2018-03-05 12:09:16 -0500

    yes

--- imageInput/src/GoogleFontAPI/sendRequest.java
+++ imageInput/src/GoogleFontAPI/sendRequest.java
@@ -1,7 +1,70 @@
 package GoogleFontAPI;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class sendRequest {
 	final protected String apikey = "AIzaSyAgJ2Fpuffe-V-3TkmdTzJwjQ9sooh7TgU";
 	
+	
+	/*public static void main(String[] args)
+	{
+		try {
+			URL url = new URL("https://crunchify.com/");
+			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+			String strTemp = "";
+			while (null != (strTemp = br.readLine())) {
+				System.out.println(strTemp);
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}*/
+	
+	private final String USER_AGENT = "Mozilla/5.0";
+
+	public static void main(String[] args) throws Exception {
+
+		sendRequest http = new sendRequest();
+
+		System.out.println("Testing 1 - Send Http GET request");
+		http.sendGet();
+
+	}
+
+	// HTTP GET request
+	private void sendGet() throws Exception {
+
+		String url = "http://www.google.com/search?q=mkyong";
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
+	}
 }
  
