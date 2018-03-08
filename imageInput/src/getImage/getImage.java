d4da14b5a0b14386776f55358177229b2663b987
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2018-03-08 12:25:27 -0500

    adjusted for specs

--- imageInput/src/getImage/getImage.java
+++ imageInput/src/getImage/getImage.java
@@ -103,16 +103,16 @@ public class getImage
 
 	
 		for(RGBValue pos2 : test.getPixels().values()) {
-			if((pos2.getR() + pos2.getG() + pos2.getB()) > sum) {
+		//	if((pos2.getR() + pos2.getG() + pos2.getB()) > sum) {
 				pos2.setR(255);
 				pos2.setB(255);
 				pos2.setG(255);
-			}
+		/*	}
 			else {
 				pos2.setR(0);
 				pos2.setB(0);
 				pos2.setG(0);
-			}
+			}*/
 		}
 		BufferedImage newimg = new BufferedImage(500,500,BufferedImage.TYPE_INT_RGB); //copy paste this line outside and
 	
