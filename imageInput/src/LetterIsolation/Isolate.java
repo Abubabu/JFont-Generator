0ca53b12026ddeea733b96c3bbd4f007aef72fe7
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2018-03-12 12:55:04 -0400

    made loop functions for isolation package

--- imageInput/src/LetterIsolation/Isolate.java
+++ imageInput/src/LetterIsolation/Isolate.java
@@ -1,5 +1,38 @@
 package LetterIsolation;
 
-public class Isolate {
+import java.util.Map;
 
+import getImage.RGBValue;
+import getImage.pixelPosition;
+
+public class Isolate {
+	
+	public static void main(String[] args)
+	{
+	
+	}
+	
+	public void HorizontalLoop(int buffer,int imageHeight, int imageWidth, Map<pixelPosition, RGBValue> pixels)
+	{
+		for(int i = 0; i < imageHeight; i = i + buffer)
+		{
+			for(int j = 0; j < imageWidth; j++)
+			{
+				RGBValue color = pixels.get(new pixelPosition(j,i));
+				System.out.println("R: "+ color.getR() + " G: " + color.getG() +" B: " + color.getB());
+			}
+		}
+	}
+	
+	public void VerticalLoop(int buffer,int imageHeight, int imageWidth, Map<pixelPosition, RGBValue> pixels)
+	{
+		for(int i = 0; i < imageWidth; i = i + buffer)
+		{
+			for(int j = 0; j < imageHeight; j++)
+			{
+				RGBValue color = pixels.get(new pixelPosition(i,j));
+				System.out.println("R: "+ color.getR() + " G: " + color.getG() +" B: " + color.getB());
+			}
+		}
+	}
 }
