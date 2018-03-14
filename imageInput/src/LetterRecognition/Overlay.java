373efa99fe4d1d1522385852fe749be709615655
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2018-03-14 13:12:44 -0400

    added overlay c9ode

--- imageInput/src/LetterRecognition/Overlay.java
+++ imageInput/src/LetterRecognition/Overlay.java
@@ -1,5 +1,37 @@
 package LetterRecognition;
 
-public class Overlay {
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import getImage.RGBValue;
+import getImage.getImage;
+
+public class Overlay {
+	public static void main(String[] args) throws IOException
+	{
+		getImage a = new getImage("a.png"); //H; 116 W: 207 ImageType: 12
+		getImage aplus = new getImage("aplus.png");
+		
+		BufferedImage negative = new BufferedImage(116,207, 12); 
+		for(int i = 0; i < 116; i++)
+		{
+			for(int j = 0; j < 207; j++)
+			{
+				if(isRGBEqual(a.getRGB(j, i),aplus.getRGB(j, i)))
+				{
+					Color a = new color()
+					negative.setRGB(j, i,  color here);
+				}
+			}
+		}
+		
+		
+	
+		
+	}
+	public static boolean isRGBEqual(RGBValue one, RGBValue two)
+	{
+		return one.getR() == two.getR() && one.getG() == two.getG() && one.getB() == two.getB();
+	}
 }
