dae465a3aee9e7d9cd32ec1e7a952c9efa47b838
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2018-03-15 13:06:05 -0400

    im god

--- imageInput/src/LetterRecognition/Overlay.java
+++ imageInput/src/LetterRecognition/Overlay.java
@@ -17,8 +17,8 @@ public class Overlay {
 		//https://stackoverflow.com/questions/29024456/image-overlay-comparison-and-pixel-color-change
 		
 		
-		getImage a = new getImage("Twitch.png"); //H; 208 W: 208 ImageType: 12
-		getImage aplus = new getImage("Twitter.png");
+		getImage a = new getImage("a.png"); //H; 208 W: 208 ImageType: 12
+		getImage aplus = new getImage("b.png");
 		
 		//System.out.println(a.getHeight() + " " + a.getWidth());
 		
@@ -27,9 +27,9 @@ public class Overlay {
 		//System.out.println(apix.length);
 		int overlap = 0;
 		int nolap = 0;
-		for(int i = 0; i < 300; i++)
+		for(int i = 0; i < 1152; i++)
 		{
-			for(int j = 0; j < 300; j++)
+			for(int j = 0; j < 648; j++)
 			{
 				//System.out.println(apix[j][i].getB() + "  " + bpix[j][i].getB());
 				//System.out.println("x:"+ i + "  y:" + j + "  " + "A:" + a.getRGB(j,i).getR() + ", " + aplus.getRGB(j,i).getR());
@@ -44,7 +44,7 @@ public class Overlay {
 			}
 		}
 		System.out.println(overlap + " " + nolap);
-		System.out.print(((double) overlap/ (double) nolap));
+		System.out.print(((double) overlap/ (double) (nolap+overlap)));
 	}
 	public static boolean isRGBEqual(RGBValue one, RGBValue two)
 	{
