9cc310a6f3a220903fdb351174df700009658424
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2018-03-15 12:27:19 -0400

    changed RGB to display proper x and y format

--- imageInput/src/getImage/getImage.java
+++ imageInput/src/getImage/getImage.java
@@ -46,7 +46,7 @@ public class getImage
 			  int red = (pixel >> 16) & 0xFF;
 			  int green = (pixel >> 8) & 0xFF;
 			  int blue = (pixel) & 0xFF;
-			  System.out.println(i + " , " + j + "    :   " + alpha + " , " + red + " , " + green + " , " + blue);
+			 System.out.println(i + " , " + j + "    :   " + alpha + " , " + red + " , " + green + " , " + blue);
 			  pixels.put(new pixelPosition(j,i), new RGBValue(alpha,red,green,blue));
 			 }
 	      }
@@ -68,7 +68,7 @@ public class getImage
 	//	System.out.println("hashmap: RGB " + pixel.get(new pixelPosition(500,500)).getR() + " " +  pixel.get(new pixelPosition(500,500)).getG() + " " + pixel.get(new pixelPosition(500,500)).getB());
 		
 		RGBValue[][] potato = test.getArrayRGB();
-		System.out.print(potato[54][12].getR() + " , " + potato[54][12].getG() + " , " + potato[54][12].getB() );
+		System.out.print(potato[436][332].getR() + " , " + potato[436][332].getG() + " , " + potato[436][332].getB() );
 	//	System.out.println(test.getRGB(0,0).getR());
 		
 		
@@ -125,14 +125,14 @@ public class getImage
 	}
 	
 	public RGBValue[][] getArrayRGB() {
-		 RGBValue[][] positions = new RGBValue[width][height];
+		 RGBValue[][] positions = new RGBValue[height][width];
 		// int arrayHeight = 0;
 		// int arrayWidth = 0;
 		 for(pixelPosition pos : this.getPixels().keySet()) {
 			/* if(arrayHeight >= height - 1) {
 				 arrayWidth++;
 			 }*/
-			 positions[pos.getxPos()][pos.getyPos()] = this.getPixels().get(pos);
+			 positions[pos.getyPos()][pos.getxPos()] = this.getPixels().get(pos);
 			 
 			 
 		 }
