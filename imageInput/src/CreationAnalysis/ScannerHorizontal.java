4f4f1659182f82c14d874498d092d91cec148e61
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2018-06-08 13:02:26 -0400

    sad

--- imageInput/src/CreationAnalysis/ScannerHorizontal.java
+++ imageInput/src/CreationAnalysis/ScannerHorizontal.java
@@ -15,6 +15,12 @@ import getImage.pixelPosition;
 
 public class ScannerHorizontal {
 	
+	public static void main(String[] args) throws IOException
+	{
+		scannerHLeft("Random/MYA.png");
+		scannerHRight("Random/MYA.png");
+	}
+	
 	public static void scannerHLeft(String filepath) throws IOException
 	{
 		getImage image = new getImage(filepath); 	
@@ -62,7 +68,7 @@ public class ScannerHorizontal {
 		getImage image = new getImage(filepath); 	
 		getImage imageConverted = new getImage(image.getBW());	
 		RGBValue[][] imagePix = imageConverted.getPixelsArray();		
-		ArrayList<pixelPosition> foundPix = new ArrayList<pixelPosition>();
+		pixelPosition[][] foundPix = new pixelPosition[imageConverted.getWidth()][imageConverted.getHeight()];
 		
 		for(int y = imageConverted.getHeight()-1; y >= 0 ; y++)
 		{
@@ -70,13 +76,33 @@ public class ScannerHorizontal {
 			{
 				if(isBlack(imagePix[x][y]))
 				{
-					foundPix.add(new pixelPosition(x,y));
+					foundPix[x][y] = new pixelPosition(x,y);
 					break;
 				}
 			}
 		}
 		
+		BufferedImage HorRight = new BufferedImage(imageConverted.getWidth(),imageConverted.getHeight(),BufferedImage.TYPE_INT_RGB);
 		
+		
+		for(int v = 0; v < imageConverted.getWidth(); v++)
+		{
+			for(int w = 0; w < imageConverted.getHeight();w++) {
+				if(foundPix[v][w] != null) {
+					int staticRed = imagePix[v][w].getR();
+					int staticGreen = imagePix[v][w].getG();
+					int staticBlue = imagePix[v][w].getB();
+					Color staticColor = new Color(staticRed,staticGreen,staticBlue);
+					HorRight.setRGB(v,w,staticColor.getRGB());
+				}
+				else
+				{
+					Color staticColor = new Color(255,255,255);
+					HorRight.setRGB(v,w,staticColor.getRGB());
+				}
+			}
+		}
+		ImageIO.write(HorRight, "bmp",new File("C:\\Users\\BT_1N3_23\\git\\JFont-Generator\\imageInput\\HorizontalRight.bmp"));	
 		
 	}
 	
