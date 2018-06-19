d6169c615c0900de4d688e0a65d70da48ff00fb8
Administrator <Administrator@MININT-DUV4C4I.home>
2018-06-18 22:27:41 -0400

    array of directions should be complete

--- imageInput/src/CreationAnalysis/LineDirections.java
+++ imageInput/src/CreationAnalysis/LineDirections.java
@@ -36,6 +36,8 @@ public class LineDirections {
 		return differences;
 	}
 	
+	private static String[] allDirections;
+	
 	public static double[][] findLines(double[] differences) {
 		int lineCount= 0;
 		String currentChange = "";
@@ -56,8 +58,56 @@ public class LineDirections {
 			}
 		}
 		double[][] allLines = new double[lineCount][differences.length];
-		
-		for(int a = 0; a < differences.length;)
+		String[] directions = new String[lineCount];
+		int currentLine = 0;
+		int currentRow = 0;
+		String currentChange2 = "";
+		for(int a = 0; a < differences.length;) {
+			if(a == 0) {
+				allLines[currentLine][currentRow] = differences[a];
+				currentRow++;	
+			}
+			if(a == 1) {
+				if(differences[a] - differences[a - 1] > 5 && !currentChange2.equals("+")) {
+					directions[currentLine] = "+";
+					currentChange2 = "+";
+				}
+				else if(differences[a] - differences[a - 1] < 5 && !currentChange2.equals("-")) {
+					directions[currentLine] = "-";
+					currentChange2 = "-";
+				}
+				else if(!currentChange2.equals("0")) {
+					directions[currentLine] = "0";
+					currentChange = "0";
+				}
+				allLines[currentLine][currentRow] = differences[a];
+				currentRow++;
+			}	
+			else if(a != 0) {
+				if(differences[a] - differences[a - 1] > 5 && !currentChange2.equals("+")) {
+					currentLine++;
+					currentRow = 0;
+					currentChange2 = "+";
+					directions[currentLine] = "+";
+				}
+				else if(differences[a] - differences[a - 1] < 5 && !currentChange2.equals("-")) {
+					currentLine++;
+					directions[currentLine] = "-";
+					currentRow = 0;
+					currentChange2 = "-";
+				}
+				else if(!currentChange2.equals("0")) {
+					currentLine++;
+					directions[currentLine] = "0";
+					currentRow = 0;
+					currentChange = "0";
+				}
+				allLines[currentLine][currentRow] = differences[a];
+				currentRow++;
+			}	
+		}
+		allDirections = directions;
+		return allLines;
 	}
 	public static boolean isBlack(RGBValue rgb)
 	{
