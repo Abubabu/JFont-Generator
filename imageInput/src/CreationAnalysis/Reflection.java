8b6715f98c10ba877b4faa68d4c9b9e35e2ad800
Sadman Hossain <39657257+Arboghast@users.noreply.github.com>
2018-06-18 20:11:42 -0400

    no

--- imageInput/src/CreationAnalysis/Reflection.java
+++ imageInput/src/CreationAnalysis/Reflection.java
@@ -14,7 +14,7 @@ import getImage.pixelPosition;
 public class Reflection {
 
 	
-	public static BufferedImage reflector(getImage img) {
+	public static BufferedImage reflectorYAxis(getImage img) {
 		BufferedImage ReflectedImg = new BufferedImage(img.getWidth(),img.getHeight(),BufferedImage.TYPE_INT_RGB);
 		RGBValue[][] colors = img.getPixelsArray();
 		for(int x = 0; x < img.getWidth(); x++) {
@@ -25,6 +25,22 @@ public class Reflection {
 				else
 				{
 					ReflectedImg.setRGB(img.getWidth()-x-1,y, new Color(255,255,255).getRGB());
+				}
+			}
+		}
+		return ReflectedImg;
+	}
+	public static BufferedImage reflectorXAxis(getImage img) {
+		BufferedImage ReflectedImg = new BufferedImage(img.getWidth(),img.getHeight(),BufferedImage.TYPE_INT_RGB);
+		RGBValue[][] colors = img.getPixelsArray();
+		for(int x = 0; x < img.getWidth(); x++) {
+			for(int y = 0; y < img.getHeight();y++) {
+				if(isBlack(colors[x][y])) {
+					ReflectedImg.setRGB(x, img.getHeight()-y-1, new Color(0,0,0).getRGB());
+				}
+				else
+				{
+					ReflectedImg.setRGB(x,img.getHeight()-y-1, new Color(255,255,255).getRGB());
 				}
 			}
 		}
--- imageInput/src/CreationAnalysis/SymmetryTest.java
+++ imageInput/src/CreationAnalysis/SymmetryTest.java
@@ -1,5 +1,33 @@
 package CreationAnalysis;
 
-public class SymmetryTest {
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+import LetterRecognition.Overlay3;
+import getImage.getImage;
+
+public class SymmetryTest {
+	public static void main(String[] args) throws IOException
+	{
+		test("Alphabet/A.png");
+	}
+
+	public static void test(String filepath) throws IOException
+	{
+			getImage image = new getImage(filepath);
+			BufferedImage YFlip = Reflection.reflectorYAxis(image);
+			BufferedImage XFlip = Reflection.reflectorXAxis(image);
+			
+			ImageIO.write(YFlip, "bmp",new File("C:\\Users\\Salehaakter\\git\\JFont-Generator\\imageInput\\YFlip.bmp"));	
+			ImageIO.write(XFlip, "bmp",new File("C:\\Users\\Salehaakter\\git\\JFont-Generator\\imageInput\\XFlip.bmp"));	
+			
+			System.out.println(filepath.substring(filepath.length()-5));
+			
+			Overlay3 symmetry = new Overlay3(image.getBW());
+			System.out.print("Verticle: " + symmetry.overlay(YFlip) + "  ");
+			System.out.println("Horizontal: " + symmetry.overlay(XFlip));
+	}
 }
